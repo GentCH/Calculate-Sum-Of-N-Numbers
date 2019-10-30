e7e3f30077a26c4b5e1bfb40e48fa95aa7b46d7b
Chin Hui <chhew96@gmail.com>
2019-10-31 01:38:48 +0800

    Create SumOfN.java

---
+++ SumOfN.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SumOfN {
+    
+    SumOfN (){
+        
+    }
+    
+    public static void main(String[] args){
+	System.out.println("");
+        System.out.print("Enter first number: ");
+        Scanner scan1 = new Scanner(System.in);
+        int first = scan1.nextInt();
+	System.out.print("Enter last number: ");
+        Scanner scan2 = new Scanner(System.in);
+        int last = scan2.nextInt();
+        calculateSum(first, last);
+        
+    }
+
+	public static void calculateSum(int firstNumber, int lastNumber){
+		int totalFirstLast = (lastNumber - firstNumber) + 1;
+		int sumN = (totalFirstLast * (totalFirstLast + 1)) / 2;
+		System.out.println("");
+		System.out.print("There are ");
+		System.out.print(totalFirstLast);
+		System.out.print(" numbesr between ");
+		System.out.print(firstNumber);
+		System.out.print(" and ");
+		System.out.print(lastNumber);
+		System.out.println("");
+		System.out.print("The sum of all numbers are ");
+		System.out.print(sumN);
+	System.out.println("");
+	}  
+}
